c61c00fe384318021054d8967be2e30544825fe4
Paula Gomez Duran <>
2019-05-13 09:39:20 +0200

    petitions

--- src/database/mysql/queries.js
+++ src/database/mysql/queries.js
@@ -1,7 +1,7 @@
 import db from './db';
 
 export const getBooking = (id, callback) => {
-    db.query(`SELECT * from RESERVATION where id = ${id}`, (error, results, fields) => {
+    db.query(`SELECT * from RESERVATION where id=${id}`, (error, results, fields) => {
         if (error) throw error;
         else
           callback(results);
